9ead6174c39f3790d8d3762884d348b51c400a23
115100 <git@three-sigma.org>
2020-12-18 12:51:51 +0000

    wp.mpd: Port to lua

---
+++ hexchat.wp.mpd.lua
@@ -0,0 +1,16 @@
+hexchat.register('MPD', '1', 'MPD currently playing script')
+
+local function get_np()
+	local handle = assert(io.popen(
+			'mpc ' ..
+			'current ' ..
+			'-h /var/lib/mpd/socket ' ..
+			'-f "%artist% - %title% 「%album%」"'
+	))
+	local res = string.gsub(assert(handle:read('*a')), '\n', '')
+	handle:close()
+	hexchat.command('SAY np: ' .. res)
+end
+
+hexchat.hook_command('wp', get_np, '"/wp" to display currently playing MPD track')
+hexchat.print('hexchat.wp.mpd.lua loaded')
