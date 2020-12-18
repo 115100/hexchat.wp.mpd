f01db28993362d88bd4834eaa7f7512d1787cc73
115100 <git@three-sigma.org>
2020-12-18 17:42:42 +0000

    wp.mpd: Return hexchat.EAT_ALL from callback

--- hexchat.wp.mpd.lua
+++ hexchat.wp.mpd.lua
@@ -10,6 +10,7 @@ local function get_np()
 	local res = string.gsub(assert(handle:read('*a')), '\n', '')
 	handle:close()
 	hexchat.command('SAY np: ' .. res)
+	return hexchat.EAT_ALL
 end
 
 hexchat.hook_command('wp', get_np, '"/wp" to display currently playing MPD track')
