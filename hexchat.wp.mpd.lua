43d65df4f364e5b1b98595d80e7013cb79f497a1
115100 <git@three-sigma.org>
2020-12-20 17:19:52 +0000

    Minor cleanup

--- hexchat.wp.mpd.lua
+++ hexchat.wp.mpd.lua
@@ -1,17 +1,27 @@
-hexchat.register('MPD', '1', 'MPD currently playing script')
+hexchat.register("MPD", "1", "MPD currently playing script")
 
 local function get_np()
-	local handle = assert(io.popen(
-			'mpc ' ..
-			'current ' ..
-			'-h /var/lib/mpd/socket ' ..
-			'-f "%artist% - %title% 「%album%」"'
-	))
-	local res = string.gsub(assert(handle:read('*a')), '\n', '')
-	handle:close()
-	hexchat.command('SAY np: ' .. res)
-	return hexchat.EAT_ALL
+    local handle =
+        assert(
+            io.popen(
+                table.concat(
+                    {
+                        "mpc",
+                        "current",
+                        "-h",
+                        "/var/lib/mpd/socket",
+                        "-f",
+                        '"%artist% - %title% 「%album%」"'
+                    },
+                    " "
+                )
+            )
+        )
+    local res = string.gsub(assert(handle:read("*a")), "\n", "")
+    handle:close()
+    hexchat.command("SAY np: " .. res)
+    return hexchat.EAT_ALL
 end
 
-hexchat.hook_command('wp', get_np, '"/wp" to display currently playing MPD track')
-hexchat.print('hexchat.wp.mpd.lua loaded')
+hexchat.hook_command("wp", get_np, '"/wp" to display currently playing MPD track')
+hexchat.print("hexchat.wp.mpd.lua loaded")
